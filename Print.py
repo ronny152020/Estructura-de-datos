7e16d194a7c673c7197830273418ae14789139cb
ronny152020 <85712824+ronny152020@users.noreply.github.com>
2021-06-10 20:39:45 -0500

    Add files via upload

---
+++ Print.py
@@ -0,0 +1,9 @@
+edad,peso=20, 80.6
+#string
+nombre="Ronny Mozo"
+Direccion="DuranValparaiso"
+Tipo_De_sexo="hombre"
+#boolean
+civil="true"
+#print
+print("mi nombre es{}, tengo{} años".format(nombre,edad))
