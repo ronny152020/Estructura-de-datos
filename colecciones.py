5b193281d054de473499baa5c94b80bebf82c147
ronny152020 <85712824+ronny152020@users.noreply.github.com>
2021-06-10 20:57:10 -0500

    Add files via upload

---
+++ colecciones.py
@@ -0,0 +1,5 @@
+#colecciones
+usuarios=("andres_74","andresmozo316@gmail.com","7894user")
+asignaturas=["EstdDatos","fisica","algebra","poo"]
+carreras={"ing":"software","lic":"educacion","medico":"cirujano"}
+print(usuarios,asignaturas,carreras)
